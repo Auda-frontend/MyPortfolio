2c4e947f4a469b81b4734ed1e55f0ae8346ec8c9
Auda <betaauda@gmail.com>
2024-10-17 23:03:22 +0200

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,44 @@
 import React from 'react';
+// images
+import Image from '../assets/avatar.svg';
+// icons
+import { FaGithub, FaYoutube, FaDribbble } from 'react-icons/fa';
+// type animation
+import { TypeAnimation } from 'react-type-animation';
+// motion
+import { motion } from 'framer-motion';
+// variants
+import { fadeIn } from '../variants'
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return <section className='section' id='home'>
+    <div className='container mx-auto'>
+      <div>
+        {/* text */}
+        <div>
+          <h1>
+            BERA <span>Auda Beta</span>
+          </h1>
+          <div>
+            <span>I am a</span>
+            <TypeAnimation sequence={[
+              'Developer',
+              2000,
+              'Designer',
+              2000,
+              'Analyst',
+              2000,
+            ]} />
+          </div>
+        </div>
+        {/* image */}
+        <div>
+          <img src={Image} alt='' />
+        </div>
+        
+      </div>
+    </div>
+  </section>;
 };
 
 export default Banner;
